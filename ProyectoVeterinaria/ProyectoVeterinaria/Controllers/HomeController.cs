ead90d128a4aba3fe32e9cad8a45f652e46d679c
Gustavo Marin <wstov@hotmail.com>
2024-08-10 09:28:11 -0600

    Home, Registro, Login
    Se crea las paginas de home, registro y login, se agrega el diseño de la web

---
+++ ProyectoVeterinaria/ProyectoVeterinaria/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using ProyectoVeterinaria.Models;
+using System.Diagnostics;
+
+namespace ProyectoVeterinaria.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Acceso()
+        {
+            return View();
+        }
+
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
