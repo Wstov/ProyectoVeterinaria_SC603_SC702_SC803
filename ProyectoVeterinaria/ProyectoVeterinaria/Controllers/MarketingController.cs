cf895ea8b6d25899eaab58af2ff651b1bbc64608
Gustavo Marin <wstov@hotmail.com>
2024-08-10 22:47:08 -0600

    Citas - Marketing
    Se crean las vistas para los módulos de Marketing y Citas

---
+++ ProyectoVeterinaria/ProyectoVeterinaria/Controllers/MarketingController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoVeterinaria.Controllers
+{
+    public class MarketingController : Controller
+    {
+        // GET: MarketingController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: MarketingController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MarketingController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MarketingController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MarketingController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MarketingController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MarketingController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MarketingController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- ProyectoVeterinaria/ProyectoVeterinaria/obj/Debug/net8.0/ProyectoVeterinaria.AssemblyInfo.cs
+++ ProyectoVeterinaria/ProyectoVeterinaria/obj/Debug/net8.0/ProyectoVeterinaria.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoVeterinaria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a7c7d39b90ea4d54c0193cb7437aa3ef3503e19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e73f2aea75975bbfcd11c5a164e0f3369de11402")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoVeterinaria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoVeterinaria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
