eb764683111a09ff23c28765d32a9592376e0509
JoseBrenesM <joseandresbrenesmontero@gmail.com>
2024-10-21 23:03:02 -0600

    Perfil usuario

---
+++ WEB/WEB/WEB/Pages/Cuenta/Eliminar.cshtml.cs
@@ -0,0 +1,81 @@
+using Abstracciones.Modelos.Persona;
+using BC;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.Json;
+
+namespace WEB.Pages.Cuenta
+{
+    public class EliminarModel : PageModel
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private Configuracion _configuracion;
+
+        [BindProperty] public Persona persona { get; set; } = default!;
+
+        public EliminarModel(Configuracion configuration, IHttpClientFactory httpClientFactory)
+        {
+            _configuracion = configuration;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<ActionResult> OnPost(Guid id)
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            string endPoint = _configuracion.ObtenerEndPoint("EliminarPersona");
+
+            //var cliente = new HttpClient();
+            var cliente = _httpClientFactory.CreateClient("ClienteVeterinaria");
+
+            var solicitud = new HttpRequestMessage(HttpMethod.Delete, string.Format(endPoint, id));
+
+            var respuesta = await cliente.SendAsync(solicitud);
+
+            if (respuesta.IsSuccessStatusCode)
+            {
+                await HttpContext.SignOutAsync();
+
+                return RedirectToPage("../Index");
+            }
+            else
+            {
+                TempData["Atención"] = "Esta persona tiene pedidos pendientes. No puedes eliminarlo.";
+                return Page();
+            }
+        }
+
+        public async Task<ActionResult> OnGet(Guid id)
+        {
+            string urlEndPoint = _configuracion.ObtenerEndPoint("ObtenerPersona");
+
+            //var cliente = new HttpClient();
+            var cliente = _httpClientFactory.CreateClient("ClienteVeterinaria");
+
+            var solicitud = new HttpRequestMessage(HttpMethod.Get, string.Format(urlEndPoint, id));
+
+            var respuesta = await cliente.SendAsync(solicitud);
+
+            if (respuesta.IsSuccessStatusCode)
+            {
+                var resultado = await respuesta.Content.ReadAsStringAsync();
+                try
+                {
+                    var options = new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    };
+
+                    persona = JsonSerializer.Deserialize<Persona>(resultado, options);
+                }
+                catch (JsonException ex)
+                {
+                    return Page();
+                }
+            }
+            return Page();
+        }
+    }
+}
